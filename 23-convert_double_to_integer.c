d2cc0604a7499ec376f72a156066cb7636bfacec
Bakare Muideen Adeleke <bakare1234@gmail.com>
2022-07-16 16:10:27 +0100

    Task completed

---
+++ 23-convert_double_to_integer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int current, futureInt;
+    double growth, futureDoub;
+    
+    scanf("%d", &current);
+    scanf("%lf", &growth);
+    futureDoub = current * (1 + growth / 100);
+    futureInt = (int) futureDoub;
+    printf("%d", futureInt);
+
+    return (0);
+}
+
+/*
+The population of a city has risen sharply over the past few years, thanks to a high birth rate. However, this poses a number of problems, including a housing shortage. The mayor has decided to deal with the problem and would like to estimate the future growth of the population, and he has called you in to help!
+
+Please write a C-program that first reads an integer representing the current population of the city, and that next reads a decimal number for the projected population growth as a percentage (either positive or negative). The program should then display the expected population of the city in a year as a whole number. By convention we will only consider "whole" people. So a population of 31.8 inhabitants will be considered as having 31 inhabitants.
+*/
