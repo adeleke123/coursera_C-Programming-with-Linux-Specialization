44c99f94645f602942c40f0bfc06a7d5a2a96f13
Bakare Muideen Adeleke <bakare1234@gmail.com>
2022-07-24 14:58:07 +0100

    Task completed

---
+++ 36-print_a_square_of_stars_using_nested_loops.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main(void)
+{
+    int number = 0;
+    scanf("%d", &number);
+    int i;
+    int x;
+    for(i = 0; i < number; i++)
+    {
+        for(x = 0; x < number; x++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+/**
+
+Create a program that displays on the screen a square of n x n stars, with the integer n given as an input.
+
+Examples
+Input:
+5
+Output:
+*****
+*****
+*****
+*****
+*****
+
+*/
